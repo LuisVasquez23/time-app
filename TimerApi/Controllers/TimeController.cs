a8fcf41df297b58fffc5b6ff1adcde48689cbda1
Luis Vasquez <vluis5457@gmail.com>
2026-04-01 21:11:40 -0600

    Iniciando

---
+++ TimerApi/Controllers/TimeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TimerApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet("today")]
+        public IActionResult GetToday()
+        {
+            var today = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+            return Ok(new { date = today });
+        }
+    }
+}
---
+++ TimerApi/Program.cs
@@ -0,0 +1,39 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 🔹 Configurar CORS
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200", "https://localhost:7149")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// 🔹 Configurar Swagger en el pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
